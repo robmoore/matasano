2fc980d27837ba6530296ab39ae75ebc93d8428b
robmoore <rob.moore@gmail.com>
2014-11-23 23:36:56 -0500

    Cleaned up using Stylish Haskell. Still can't resolve issue with challenge-8.hs.

--- challenge-7.hs
+++ challenge-7.hs
@@ -2,12 +2,12 @@
 -- "YELLOW SUBMARINE". Decrypt it. You know the key, after all.
 -- http://cryptopals.com/sets/1/challenges/7/
 
-import qualified Crypto.Cipher.Types as CT
-import qualified Crypto.Cipher.AES as CA
-import qualified Data.ByteString.Char8 as BC
-import qualified Data.ByteString as B
+import qualified Crypto.Cipher.AES      as CA
+import qualified Crypto.Cipher.Types    as CT
+import qualified Data.ByteString        as B
 import qualified Data.ByteString.Base64 as BB (decode)
-import qualified Data.Char as DC (ord)
+import qualified Data.ByteString.Char8  as BC
+import qualified Data.Char              as DC (ord)
 
 keyString :: B.ByteString
 keyString = BC.pack "YELLOW SUBMARINE"
@@ -16,19 +16,19 @@ initAES128 :: B.ByteString -> CA.AES
 initAES128 = either (error . show) CT.cipherInit . CT.makeKey
 
 decrypt :: CA.AES -> BC.ByteString -> BC.ByteString
-decrypt ctx msg =
-	either (error . show) (CA.decryptECB ctx) decodedMsg
-	where decodedMsg = BB.decode msg
+decrypt ctx msg = either (error . show) (CA.decryptECB ctx) decodedMsg
+	    where decodedMsg = BB.decode msg
 
 -- Strip off padding per method used in [PKCS5], [PKCS7], and [CMS].
 -- See section "method 1" at http://www.di-mgt.com.au/cryptopad.html
 stripPadding :: B.ByteString -> B.ByteString
-stripPadding bs = if (isPadded) then BC.take offset bs else bs 
+stripPadding bs = if isPadded then BC.take offset bs else bs
         where paddingCount = BC.last bs
               offset = BC.length bs - DC.ord paddingCount
               isPadded = BC.all (== paddingCount) (BC.drop offset bs)
 
-main = do 
+main :: IO ()
+main = do
         file <- BC.readFile "7.txt"
         let pText = decrypt ctx $ BC.concat $ BC.lines file
         putStr $ BC.unpack $ stripPadding pText
--- challenge-8.hs
+++ challenge-8.hs
@@ -7,43 +7,45 @@
 -- Remember that the problem with ECB is that it is stateless and deterministic; the same 16 byte plaintext
 -- block will always produce the same 16 byte ciphertext.
 
--- hamming distance average over 16 byte chunks for each string
+-- (133,2.499671052631579)
 
 {-# LANGUAGE OverloadedStrings #-}
 
-import qualified Data.Bits as DB (xor, popCount)
-import qualified Data.ByteString as BS (ByteString, empty, drop, take, zipWith)
-import qualified Data.ByteString.Char8 as BC (readFile, lines)
+import           Control.Applicative
+import qualified Data.Bits              as DB (popCount, xor)
+import qualified Data.ByteString        as BS (ByteString, drop, empty, take,
+                                               zipWith)
 import qualified Data.ByteString.Base16 as B16 (decode)
-import qualified Data.List as DL (map, genericLength, minimumBy, zip)
-import qualified Data.Ord as DO (comparing)
+import qualified Data.ByteString.Char8  as BC (lines, readFile)
+import qualified Data.List              as DL (genericLength, map, minimumBy,
+                                               zip)
+import qualified Data.Ord               as DO (comparing)
 
 evalLine :: Fractional a => BS.ByteString -> a
 evalLine line = realToFrac (sum hds) / DL.genericLength hds
   where
    combos = combine $ splitBytes line
-   xordCombos = map (\x -> BS.zipWith DB.xor (fst x) (snd x)) combos
-   hds = concat $ map (\x -> DL.map DB.popCount x) xordCombos
+   xordCombos = map (uncurry $ BS.zipWith DB.xor) combos
+   hds = concatMap (DL.map DB.popCount) xordCombos
 
 -- TODO: Maybe we should be breaking up into 8s?
 splitBytes :: BS.ByteString -> [BS.ByteString]
-splitBytes bs = split bs [] 
-   where split rbs acc 
+splitBytes bs = split bs []
+   where split rbs acc
            | BS.empty == rbs = acc
-           | otherwise = split (BS.drop 16 rbs) (BS.take 16 rbs : acc) 
+           | otherwise = split (BS.drop 16 rbs) (BS.take 16 rbs : acc)
 
--- Combines list contents into unique pairs -- 
+-- Combines list contents into unique pairs --
 -- ["A","B","C","D"] -> [("A","B") ("A","C") ("A","D") ("B","C") ("B","D") ("C","D")]
 combine :: [a] -> [(a, a)]
 combine xs = combine' xs []
-  where 
-  	pair y ys = DL.map (\s -> (y, s)) ys
-  	combine' [] acc = acc
-  	combine' (z:zs) acc = combine' zs (acc ++ (pair z zs))
+  where
+    pair y = DL.map (\s -> (y, s))
+    combine' [] acc = acc
+    combine' (z:zs) acc = combine' zs (acc ++ pair z zs)
 
-main = do 
-        file <- BC.readFile "8.txt"
-        let lowestHd = DL.minimumBy (DO.comparing snd) $ hdMeans file
-        putStrLn ("Line # " ++ fst lowestHd ++ "with a score of " ++ snd lowestHd)
-       where 
-        hdMeans fl = DL.zip [1..] $ DL.map evalLine $ BC.lines fl
+main :: IO ()
+main = do
+ hdMeans <- DL.map evalLine <$> BC.lines <$> BC.readFile "8.txt"
+ let lowestHd = DL.minimumBy (DO.comparing snd) $ DL.zip [1..] hdMeans
+ putStrLn $ "Line # " ++ fst lowestHd ++ "with a score of " ++ snd lowestHd
